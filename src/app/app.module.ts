b1f9016480f490975e3932f02f77f02e079c7d7e
Jonatas <jonatasge@outlook.com>
2019-02-24 15:25:51 -0300

    Added dependency "ngx-envconfig" & preconfigurations for SpotifyService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+// ANGULAR
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchComponent } from './components/search/search.component';
-import { BgAnimationComponent } from './components/bg-animation/bg-animation.component';
-
+// ANGULAR - MATERIAL
 import {
   MatCardModule,
   MatInputModule,
   MatSelectModule,
   MatButtonModule
 } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// VENDORS
+import { ConfigModule } from 'ngx-envconfig';
+import { environment } from '../../src/environments/environment';
+
+// MY ITEMS
+// APP COMPONENT
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// OTHERS COMPONENTS
+import { BgAnimationComponent } from './components/bg-animation/bg-animation.component';
+import { SearchComponent } from './components/search/search.component';
 @NgModule({
   declarations: [AppComponent, SearchComponent, BgAnimationComponent],
   imports: [
@@ -25,7 +33,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ConfigModule.forRoot(environment)
   ],
   providers: [],
   bootstrap: [AppComponent]
